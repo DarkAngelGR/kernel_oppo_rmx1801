c76c95fe0ecb8e1a9c99815569dd150efd478fd7
DarkAngelGR <asavvo01@gmail.com>
2019-10-13 13:19:13 +0000

    Fix compilation

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1366,7 +1366,7 @@ emsgsize:
 	     (skb && skb_is_gso(skb))) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&
 	    (rt->dst.dev->features & NETIF_F_UFO) && !dst_xfrm(&rt->dst) &&
-	    (sk->sk_type == SOCK_DGRAM) && !udp_get_no_check6_tx(sk))) {
+	    (sk->sk_type == SOCK_DGRAM) && !udp_get_no_check6_tx(sk)) {
 		err = ip6_ufo_append_data(sk, queue, getfrag, from, length,
 					  hh_len, fragheaderlen, exthdrlen,
 					  transhdrlen, mtu, flags, fl6);
